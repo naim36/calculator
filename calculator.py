7012b235f2b537cd7e04c2f0d0084148ca033b56
naim36 <100689077+naim36@users.noreply.github.com>
2023-11-30 04:25:07 +0600

    first commit

---
+++ calculator.py
@@ -0,0 +1,49 @@
+# now i am gonna show u a code of calculator by pyhton language
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def sub(num1, num2):
+    return num1 - num2
+
+
+def mult(num1, num2):
+    return num1 * num2
+
+
+def div(num1, num2):
+    return num1 / num2
+
+
+def main():
+
+    num1 = float(input("enter ur first value : "))
+    operator = input("Enter any oparetor + , - , * , / : ")
+    num2 = float(input("enter ur second value : "))
+
+    if operator == "+":
+        result = add(num1, num2)
+
+    elif operator == "*":
+        result = mult(num1, num2)
+
+    elif operator == "-":
+        result = sub(num1, num2)
+
+    elif operator == "/":
+        result = div(num1, num2)
+
+    else:
+        print("cheack koro vul ache")
+        return
+
+    print(f" {num1} {operator} {num2} = {result} ")
+
+
+if __name__ == "__main__":
+    main()
+
+print("thanks a lot")
+
+
